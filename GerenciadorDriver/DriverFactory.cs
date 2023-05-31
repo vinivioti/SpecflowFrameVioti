fafcc8204e4d2720af5aace06fe3b74016a22572
vinivioti <vinivioti@yahoo.com.br>
2023-05-31 13:50:46 -0300

    Atualização DriverFactory e TakeScreenShot

--- GerenciadorDriver/DriverFactory.cs
+++ GerenciadorDriver/DriverFactory.cs
@@ -1,37 +1,86 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using WebDriverManager.DriverConfigs.Impl;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
 using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace FrameVioti.GerenciadorDriver
 {
     public class DriverFactory
     {
-        private static WebDriver driver;
-
-
+        private static IWebDriver driver;
 
         private DriverFactory() { }
-        public static WebDriver GetDriver()
+
+        //public static IWebDriver GetDriver(BrowserType browserType = BrowserType.Chrome)
+        //  public static IWebDriver GetDriver(BrowserType browserType = BrowserType.Edge)
+        //  public static IWebDriver GetDriver(BrowserType browserType = BrowserType.Firefox)
+        //  public static IWebDriver GetDriver(BrowserType browserType = BrowserType.Safari)
+          public static IWebDriver GetDriver(BrowserType browserType = BrowserType.Opera)
+
+
         {
             if (driver == null)
             {
-                new DriverManager().SetUpDriver(new ChromeConfig());
-                ChromeOptions options = new ChromeOptions();
-               // options.AddArgument("--headless"); // Adiciona o argumento para modo headless (sem abrir a tela)
-                driver = new ChromeDriver(options);
+                switch (browserType)
+                {
+                    case BrowserType.Chrome:
+                        new DriverManager().SetUpDriver(new ChromeConfig());
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        // chromeOptions.AddArgument("--headless"); // Adicione o argumento para o modo headless (sem abrir a tela)
+                        driver = new ChromeDriver(chromeOptions);
+                        break;
+
+                    case BrowserType.Edge:
+                        new DriverManager().SetUpDriver(new EdgeConfig());
+                        EdgeOptions edgeOptions = new EdgeOptions();
+                        // edgeOptions.AddArgument("--headless"); // Adicione o argumento para o modo headless (sem abrir a tela)
+                        driver = new EdgeDriver(edgeOptions);
+                        break;
+
+                    case BrowserType.Firefox:
+                        new DriverManager().SetUpDriver(new FirefoxConfig());
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                       // firefoxOptions.AddArgument("--headless"); // Adicione o argumento para o modo headless (sem abrir a tela)
+                        driver = new FirefoxDriver(firefoxOptions);
+                        break;
+
+                    case BrowserType.Safari:
+                       // new DriverManager().SetUpDriver(new SafariConfig()); -> Não existe essa opção .. estudar melhor isso.
+                        driver = new SafariDriver();
+                        break;
+
+
+                 
+                        
+                    /*  
+                     *  
+                    //#### AJUSTAR NO FUTURO PARA O OPERAGX ###
+                    
+                      case BrowserType.Opera:
+                          new DriverManager().SetUpDriver(new ChromeConfig()); // Usando o driver do Chrome para o Opera
+                          ChromeOptions operaOptions = new ChromeOptions();
+                          operaOptions.BinaryLocation = "C:\\Users\\viniv\\source\\PessoalSpec\\FrameViotiSpecflow\\bin\\Debug\\net6.0\\Opera"; // Especifique o caminho para o executável do Opera
+                          //operaOptions.AddArgument("--headless"); // Adicione o argumento para o modo headless (sem abrir a tela)
+                          driver = new ChromeDriver(operaOptions);
+                          break;
+                       
+                    */
+
+
+                    default:
+                        // Caso o tipo de navegador não seja suportado, lançar uma exceção ou definir um comportamento padrão
+                        throw new System.NotSupportedException($"Tipo de navegador '{browserType}' não suportado.");
+                }
+
                 driver.Manage().Window.Maximize();
-
-
             }
+
             return driver;
         }
-
 
         public static void KillDriver()
         {
@@ -41,9 +90,15 @@ namespace FrameVioti.GerenciadorDriver
                 driver = null;
             }
         }
+    }
 
-
-
+    public enum BrowserType
+    {
+        Chrome,
+        Edge,
+        Firefox,
+        Safari,
+        Opera,
+        IE
     }
 }
-
--- Support/PrintaTela.cs
+++ Support/PrintaTela.cs
@@ -4,22 +4,18 @@ using NPOI.SS.Util;
 using OpenQA.Selenium;
 using System.IO;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FrameVioti.Support
 {
     public class PrintaTela
     {
-        public void TakeScreenshot()
+        public void TakeScreenshot(BrowserType browserType = BrowserType.Chrome)
         {
             try
             {
                 // ***** Salva na pasta Evidencias dentro da pasta bin->Debug->net.6.0 *** //
 
-                // Obter o diretório base do projeto 
+                // Obter o diretório base do projeto
                 string pastaProjeto = AppDomain.CurrentDomain.BaseDirectory;
 
                 // Construir o caminho para a pasta Evidencias
@@ -29,13 +25,15 @@ namespace FrameVioti.Support
                 if (!Directory.Exists(pastaEvidencias))
                     Directory.CreateDirectory(pastaEvidencias);
 
-                Screenshot ss = DriverFactory.GetDriver().GetScreenshot();
+                using (IWebDriver driver = DriverFactory.GetDriver(browserType))
+                {
+                    Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
 
-                DateTime dataAtual = DateTime.Now;
-                string filename = dataAtual.ToString("dd-MM-yyyy-hhmmss");
-                string caminhoArquivo = Path.Combine(pastaEvidencias, $"Evidencia-{filename}.png");
-                ss.SaveAsFile(caminhoArquivo, ScreenshotImageFormat.Png);
-
+                    DateTime dataAtual = DateTime.Now;
+                    string filename = dataAtual.ToString("dd-MM-yyyy-hhmmss");
+                    string caminhoArquivo = Path.Combine(pastaEvidencias, $"Evidencia-{filename}.png");
+                    ss.SaveAsFile(caminhoArquivo, ScreenshotImageFormat.Png);
+                }
             }
             catch (Exception e)
             {
@@ -45,5 +43,3 @@ namespace FrameVioti.Support
         }
     }
 }
-
-
