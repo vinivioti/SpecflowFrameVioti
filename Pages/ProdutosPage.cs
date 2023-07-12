0f18782232aba2f27c80aff99e936bc3fb9d746b
vinivioti <vinivioti@yahoo.com.br>
2023-07-12 10:17:38 -0300

    manutenção projeto exemplo GerenciadorFiles

--- Pages/ProdutosPage.cs
+++ Pages/ProdutosPage.cs
@@ -15,8 +15,40 @@ namespace FrameVioti.Pages
     [Binding]
     public class ProdutosPage : ProdutosElementos
     {
-        private PrintaTela print = new PrintaTela();
+        		
+		  //########################### INICIO DO EXEMPLO DE INSERÇÃO ARQUIVOS #######################################
 
+        private string ANEXO;
+
+        //Exemplo para trabalhar com subida de arquivos na tela do DOM
+        public ProdutosPage GerenciamentoArquivos()
+        {
+            //Para setar o caminho genérico para uma pasta (GerenciadorFiles) dentro do projeto mas fora da \bin\Debug
+            string projetoPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", ".."));
+            string gerenciadorFilesPath = Path.Combine(projetoPath, "GerenciadorFiles");
+
+            ANEXO = gerenciadorFilesPath;
+
+            return this;
+
+        }
+
+        public ProdutosPage ExemploDeUsoArquivos()
+        {
+            //Exemplo de uso:
+            var wait = new WebDriverWait(DriverFactory.GetDriver(), TimeSpan.FromSeconds(20));
+            wait.Until((d) => d.FindElement(GetBtnCheckout()) != null);
+            DriverFactory.GetDriver().FindElement(GetBtnCheckout()).SendKeys(Path.Combine(ANEXO, "ArquivoDesejado_xx-xx-2023.csv"));
+            Console.WriteLine("Anexou com sucesso!!");
+
+            return this;
+
+        }
+						//###### NÃO ESQUEÇA DE CRIAR A PASTA DESEJADA NO PROJETO !!!!!. #####
+    //########################### FIM DO EXEMPLO ACIMA #######################################
+
+
+			private PrintaTela print = new PrintaTela();
 
         public ProdutosPage FiltroSelecaoProduto()
         {
