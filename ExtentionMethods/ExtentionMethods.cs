aa032189adb6a7ac2ab96d999cabc15ab715aee3
Vioti <82620421+vinivioti@users.noreply.github.com>
2024-09-09 16:00:47 -0300

    Update ExtentionMethods.cs
    Updadte para que o Report possa anexar evidencias de teste

--- ExtentionMethods/ExtentionMethods.cs
+++ ExtentionMethods/ExtentionMethods.cs
@@ -1,79 +1,103 @@
-﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports;
 using AventStack.ExtentReports.Gherkin.Model;
+using FrameVioti.GerenciadorDriver;
+using FrameVioti.Support;
+using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow.Bindings;
 
 namespace FrameVioti.ExtentionMethods
 {
     public static class ScenarioExtensionMethod
     {
-        // aqui é um método para criar um Scenario passando o tipo de Step
         private static ExtentTest CreateScenario(ExtentTest extent, StepDefinitionType stepDefinitionType)
         {
-            // o SpecFlow nos permite pegar o nome do Step usando o ScenarioStepContext.Current
             var scenarioStepContext = ScenarioStepContext.Current.StepInfo.Text;
+            ExtentTest stepNode = null;
 
             switch (stepDefinitionType)
             {
                 case StepDefinitionType.Given:
-                    return extent.CreateNode<Given>(scenarioStepContext); // cria o nodo para Given
-
-                case StepDefinitionType.Then:
-                    return extent.CreateNode<Then>(scenarioStepContext); // cria o nodo para Then
+                    stepNode = extent.CreateNode<Given>(scenarioStepContext);
+                    break;
 
                 case StepDefinitionType.When:
-                    return extent.CreateNode<When>(scenarioStepContext); // cria o nodo para When
+                    stepNode = extent.CreateNode<When>(scenarioStepContext);
+                    break;
+
+                case StepDefinitionType.Then:
+                    stepNode = extent.CreateNode<Then>(scenarioStepContext);
+                    break;
+
                 default:
                     throw new ArgumentOutOfRangeException(nameof(stepDefinitionType), stepDefinitionType, null);
             }
+
+            // Adicionar a captura de tela como Base64 logo após criar o passo
+            AddScreenshotAsBase64(stepNode, stepNode.Model.Name);
+            return stepNode;
         }
 
-        // aqui temos um método para criar um novo de falha ou erro
         private static void CreateScenarioFailOrError(ExtentTest extent, StepDefinitionType stepDefinitionType)
         {
             var error = ScenarioContext.Current.TestError;
 
-            // se não existir exception então pega a mensagem de erro do ScenarioContext.Current
             if (error.InnerException == null)
             {
-                CreateScenario(extent, stepDefinitionType).Fail(error.Message);
+                CreateScenario(extent, stepDefinitionType).Error(error.Message);
             }
             else
             {
-                // Se houver exceção interna, pegue a mensagem de erro dela
-            var errorMessage = error.InnerException.Message;
-            CreateScenario(extent, stepDefinitionType).Fail(errorMessage);
+                CreateScenario(extent, stepDefinitionType).Fail(error.InnerException);
             }
         }
 
-        // os métodos abaixo só facilitei as chamadas para Given, When e Then
         public static void StepDefinitionGiven(this ExtentTest extent)
         {
             if (ScenarioContext.Current.TestError == null)
+            {
                 CreateScenario(extent, StepDefinitionType.Given);
+            }
             else
+            {
                 CreateScenarioFailOrError(extent, StepDefinitionType.Given);
+            }
         }
 
         public static void StepDefinitionWhen(this ExtentTest extent)
         {
             if (ScenarioContext.Current.TestError == null)
+            {
                 CreateScenario(extent, StepDefinitionType.When);
+            }
             else
+            {
                 CreateScenarioFailOrError(extent, StepDefinitionType.When);
+            }
         }
 
         public static void StepDefinitionThen(this ExtentTest extent)
         {
             if (ScenarioContext.Current.TestError == null)
+            {
                 CreateScenario(extent, StepDefinitionType.Then);
+            }
             else
+            {
                 CreateScenarioFailOrError(extent, StepDefinitionType.Then);
+            }
         }
+
+        // Método auxiliar para capturar e anexar screenshot como Base64
+        private static void AddScreenshotAsBase64(ExtentTest extent, string stepName)
+        {
+            var printaTela = new PrintaTela();
+            string screenshotBase64 = printaTela.CaptureScreenshotAsBase64();
+            extent.AddScreenCaptureFromBase64String(screenshotBase64, $"Screenshot_{stepName}");
+
+            string imgHtml = $"<img src='data:image/png;base64,{screenshotBase64}' width='300' height='150' />";
+            extent.Log(Status.Pass, "Evidência: " + imgHtml); // Ajuste para adicionar como uma miniatura
+        }
+
     }
 }
-
